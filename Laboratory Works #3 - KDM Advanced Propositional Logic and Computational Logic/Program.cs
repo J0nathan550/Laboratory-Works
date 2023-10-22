78aebcc39d4935a5bc4f7c50f20448717ee9ed99
J0nathan550 <ihudokormov@gmail.com>
2023-10-22 15:44:16 +0200

    fixed few spelling mistakes

--- Laboratory Works #3 - KDM Advanced Propositional Logic and Computational Logic/Program.cs
+++ Laboratory Works #3 - KDM Advanced Propositional Logic and Computational Logic/Program.cs
@@ -1,23 +1,19 @@
-﻿namespace Laboratory_Works__3___KDM_Advanced_Propositional_Logic_and_Computational_Logic
+﻿public class Program
 {
-    public class Program
+    private static void Main(string[] args)
     {
-        private static void Main(string[] args)
-        {
-            Console.WriteLine("Task 1 (Complex Logical Expressions Evaluation):");
+        Console.WriteLine("Task 1 (Complex Logical Expressions Evaluation):");
 
-            string expression = "(A AND B) OR (C AND D)";
-            Dictionary<string, bool> dictionary = new Dictionary<string, bool>();
-            dictionary.Add("A", true);
-            dictionary.Add("B", false);
-            dictionary.Add("C", true);
-            dictionary.Add("D", true);
+        string expression = "(A AND B) OR (NOT C)";
+        Dictionary<string, bool> dictionary = new Dictionary<string, bool>();
+        dictionary.Add("A", true);
+        dictionary.Add("B", false);
+        dictionary.Add("C", true);
 
-            Console.WriteLine(Logic_Evaluator.ExpressionEvaluation(expression, dictionary) + "\n");
+        Console.WriteLine(Logic_Evaluator.ExpressionEvaluation(expression, dictionary) + "\n");
 
-            Console.WriteLine("Task 2: Automated Truth Table Generation:" + "\n");
+        Console.WriteLine("Task 2: Automated Truth Table Generation:" + "\n");
 
-            Console.WriteLine(Logic_Evaluator.TruthTable(expression));
-        }
+        Console.WriteLine(Logic_Evaluator.TruthTable(expression));
     }
 }
